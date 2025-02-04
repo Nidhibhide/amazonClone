466cfb431eb6d09523eaa0861e18827b38502e1a
Nidhibhide <nidhibhide144@gmail.com>
2025-02-04 12:18:12 +0530

    delete unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-
-import './App.css'
-
 function App() {
-
-
   return (
     <>
-      <h1 className='bg-pink-300 font-bold'>heyy</h1>
+      <h1 className="bg-blue-300 font-bold">heyy</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
